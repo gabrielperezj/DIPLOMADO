29902e97a770be0d513b342e50124815f006c9cc
gabrielperezj <gabrielperezjimenez28divine@gmail.com>
2019-02-23 09:21:30 -0600

    archivo final

--- ejercicio_robot1/ejercicio_robot1.ino
+++ ejercicio_robot1/ejercicio_robot1.ino
@@ -12,45 +12,40 @@ void setup() {
 void loop() {
 
     adelante();
-    delay(2000);
+    delay(1300);
+    detenido();
+    delay(600);
     giroIzq();
-    delay(200);
+    delay(600);
+    detenido();
+    delay(600);
     adelante();
     delay(1000);
+    detenido();
+    delay(600);
     giroIzq();
+    delay(670);
+    detenido();
+    delay(600);
     adelante();
-    delay(1000);
+    delay(1500);
+    detenido();
+    delay(600);
     giroIzq();
+    delay(670);
+    detenido();
+    delay(600);
+    adelante();
+    delay(900);
+    detenido();
+    delay(600);
+    giroIzq();
+    delay(670);
     
-    
-
+       
 }
 
 void adelante()
-{
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
-  analogWrite(3, 100);
-
-  digitalWrite(13, LOW);
-  digitalWrite(12, HIGH);
-  analogWrite(5, 100);  
-  
-}
-
-void giroIzq()
-{
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
-  analogWrite(3, 100);
-
-  digitalWrite(13, LOW);
-  digitalWrite(12, HIGH);
-  analogWrite(5, 50); 
-  
-}
-
-void giroDer()
 {
   digitalWrite(7, LOW);
   digitalWrite(8, HIGH);
@@ -58,7 +53,31 @@ void giroDer()
 
   digitalWrite(13, LOW);
   digitalWrite(12, HIGH);
-  analogWrite(5, 100); 
+  analogWrite(5, 65);  
+  
+}
+
+void giroIzq()
+{
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  analogWrite(3, 50);
+
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  analogWrite(5, 25); 
+  
+}
+
+void giroDer()
+{
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  analogWrite(3, 25);
+
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  analogWrite(5, 50); 
   
 }
 
